654c0e9f83ca44fb275d5815db62cc94a3dd4008
Spencer <131034335+CodingSpencer@users.noreply.github.com>
2025-07-13 00:51:05 -0600

    firebase added

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
-import { initAddDeviceForm } from "./functions/deviceSearch.js";
-import { handleDeviceRecommendation } from "./functions/recommendDevices.js";
+import '../assets/css/style.css'; // CSS gets bundled by Vite
 
-initAddDeviceForm();
+import { initAddDeviceForm } from './functions/deviceSearch.js';
+import { handleDeviceRecommendation } from './functions/recommendDevices.js';
+
+// Initialize functions after DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+  initAddDeviceForm();
+  handleDeviceRecommendation();
+});
